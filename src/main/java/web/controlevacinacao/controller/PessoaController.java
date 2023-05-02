7df30a14fcaf409140a860ea3f356f7abee4d960
Rose Gates <pedroluiz2109@gmail.com>
2023-05-02 09:42:28 -0300

    add: pessoa view

--- src/main/java/web/controlevacinacao/controller/PessoaController.java
+++ src/main/java/web/controlevacinacao/controller/PessoaController.java
@@ -1,5 +1,7 @@
 package web.controlevacinacao.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import web.controlevacinacao.model.Pessoa;
+import web.controlevacinacao.model.Status;
 import web.controlevacinacao.model.filter.PessoaFilter;
 import web.controlevacinacao.pagination.PageWrapper;
 import web.controlevacinacao.repository.PessoaRepository;
@@ -24,10 +27,10 @@ import web.controlevacinacao.service.PessoaService;
 public class PessoaController {
 
     @Autowired
-    private PessoaRepository vacinaRepository;
+    private PessoaRepository pessoaRepository;
 
     @Autowired
-    private PessoaService vacinaService;
+    private PessoaService pessoaService;
 
 
     @GetMapping("/cadastrar")
@@ -40,7 +43,7 @@ public class PessoaController {
 
     @PostMapping("/cadastrar")
     public String cadastrar(Pessoa pessoa) {
-        vacinaService.salvar(pessoa);
+        pessoaService.salvar(pessoa);
         return"redirect:/pessoas/cadastrook";
     }
 
@@ -52,7 +55,7 @@ public class PessoaController {
 
     @GetMapping("/abrirpesquisar")
     public String abrirPesquisar(){
-        return "vacinas/pesquisar";
+        return "pessoas/pesquisar";
     }
 
     @GetMapping("/pesquisar")
@@ -61,15 +64,63 @@ public class PessoaController {
             @SortDefault(sort = "codigo", direction = Sort.Direction.ASC) Pageable pageable,
             HttpServletRequest request){
         
-        Page<Pessoa> pagina = vacinaRepository.buscarComFiltro(filtro, pageable);
+        Page<Pessoa> pagina = pessoaRepository.buscarComFiltro(filtro, pageable);
         if(!pagina.isEmpty()){
             PageWrapper<Pessoa> paginaWrapper = new PageWrapper<>(pagina, request);
             model.addAttribute("pagina", paginaWrapper);
             
-            return "vacinas/vacinas";
+            return "pessoas/pessoas";
         } else {
             model.addAttribute("mensagem", "Nenhuma pessoa com estas informações");
             return "mostrarmensagem";
         }
+    }
+
+    @PostMapping("/abriralterar")
+    public String abrirAlterar(Pessoa pessoa, Model model) {
+        model.addAttribute("titulo", "Alterar Pessoa");
+        model.addAttribute("url", "/pessoas/alterar");
+        model.addAttribute("textoBotao", "Alterar");
+        return "pessoas/cadastrar";
+    }
+
+    @PostMapping("/alterar")
+    public String alterar(Pessoa pessoa) {
+        pessoaService.salvar(pessoa);
+        return "redirect:/pessoas/alteracaook";
+    }
+
+    @GetMapping("/alteracaook")
+    public String mostrarMensagemAlteracaoOK(Model model) {
+        model.addAttribute("mensagem", "Pessoa alterada com sucesso");
+        return "mostrarmensagem";
+    }
+
+    @PostMapping("/abrirremover")
+    public String abrirRemover(Pessoa pessoa){
+        return "pessoas/confirmarremocao";
+    }
+    
+    @PostMapping("/remover")
+    public String remover(Long codigo, Model model){
+        Optional<Pessoa> optPessoa = pessoaRepository.findById(codigo);
+        if(optPessoa.isPresent()){
+            Pessoa pessoa = optPessoa.get();
+            pessoa.setStatus(Status.INATIVO);
+            pessoaService.salvar(pessoa);
+
+            return "redirect:/pessoas/remocaook";
+        } else {
+            model.addAttribute("mensagem", "nenhuma pessoa foi encontrada com este codigo");
+            return "mostrarmensagem";
+        }
+
+        
+    }
+
+    @GetMapping("/remocaook")
+    public String mostrarMensagemRemocaoOK(Model model) {
+        model.addAttribute("mensagem", "Pessoa removida com sucesso");
+        return "mostrarmensagem";
     }
 }
